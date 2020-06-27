6a278ec72d6c0a8ab027fd2c69b8aa27991f8ac4
Ray-Garraty <vl.besed@gmail.com>
2020-06-27 18:13:52 +0300

    added tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,26 +1,38 @@
 import { beforeAll, test, expect } from '@jest/globals';
 import path from 'path';
+import { dirname } from 'path';
 import fs from 'fs';
 import genDiff from '../index';
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 let pathToBeforeFile;
 let pathToAfterFile;
-let resultFile;
+let result;
+let answer;
+let testNumber;
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), "utf8");
+const getResult = (pathToBeforeFile, pathToAfterFile) => genDiff(pathToBeforeFile, pathToAfterFile).split('\n').join('').trim();
+const getAnswer = (filename) => readFile(filename).split('\n').join('').trim();
 
-beforeAll(() => {
-  pathToBeforeFile = getFixturePath('before.json');
-  pathToAfterFile = getFixturePath('after.json');
-  resultFile = readFile('result.json');
+test('Processing first pair of files...', () => {
+  testNumber = 1;
+  pathToBeforeFile = getFixturePath(`before${testNumber}.json`);
+  pathToAfterFile = getFixturePath(`after${testNumber}.json`);
+  result = getResult(pathToBeforeFile, pathToAfterFile);
+  answer = getAnswer(`answer${testNumber}.json`);
+  expect(result).toBe(answer);
 });
 
-test('Checking genDiff function...', () => {
-  expect(genDiff(pathToBeforeFile, pathToAfterFile)).toBe(resultFile);
+test('Processing second pair of files...', () => {
+  testNumber = 2;
+  pathToBeforeFile = getFixturePath(`before${testNumber}.json`);
+  pathToAfterFile = getFixturePath(`after${testNumber}.json`);
+  result = getResult(pathToBeforeFile, pathToAfterFile);
+  answer = getAnswer(`answer${testNumber}.json`);
+  expect(result).toBe(answer);
 });
