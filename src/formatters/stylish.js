d59dfd5fce1cb241582c2631a84db4388522389b
Ray-Garraty <vl.besed@gmail.com>
2020-07-25 16:13:39 +0300

    changed the stylish formatter algorithm once again...

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,45 +1,78 @@
 import _ from 'lodash';
 
-const createIndent = (n) => '  '.repeat(n);
-
-const iter = (node, depth) => {
-  const {
-    name,
-    type,
-    value,
-    value1,
-    value2,
-    children,
-  } = node;
-  const prefixes = {
-    added: '+ ',
-    removed: '- ',
-    same: '  ',
-  };
-  const increment = 2;
-  const indent = createIndent(depth);
-  const closingSequence = `\n${indent}  }`;
-  if (!type) {
-    const result = [];
-    _.forOwn(node, (val, key) => result.push(`\n${indent}${prefixes.same}${key}: ${val}`));
-    return result;
-  }
-  const formatValue = (val) => (_.isPlainObject(val) ? `{${iter(val, depth + increment)}${closingSequence}` : val);
-  switch (type) {
-    case 'added':
-    case 'removed':
-    case 'same':
-      return `${indent}${prefixes[type]}${name}: ${formatValue(value)}`;
-    case 'differs': {
-      const firstLine = `${indent}${prefixes.removed}${name}: ${formatValue(value1)}`;
-      const secondLine = `${indent}${prefixes.added}${name}: ${formatValue(value2)}`;
-      return [firstLine, secondLine].join('\n');
-    }
-    case 'parent':
-      return `${indent}${prefixes.same}${name}: {\n${children.flatMap((child) => iter(child, depth + increment)).join('\n')}${closingSequence}`;
-    default:
-      throw new Error(`Unexpected node type: ${type}`);
-  }
+const offset = 2;
+const separator = '  ';
+const openSymbol = '{';
+const closingSymbol = '}';
+const lineBreakSymbol = '\n';
+const keyMarkers = {
+  same: '',
+  parent: '',
+  added: '+',
+  removed: '-',
 };
 
-export default (data) => `{\n${data.flatMap((node) => iter(node, 1)).join('\n')}\n}`;
+const createIndent = (n) => '  '.repeat(n);
+
+const createPrefix = (name, type, depth) => {
+  const indent = createIndent(depth);
+  const marker = keyMarkers[type].padEnd(2, ' ');
+  return `${indent}${marker}${name}`;
+};
+
+const createEnding = (depth) => {
+  const indent = createIndent(depth);
+  return [indent, closingSymbol].join(separator);
+};
+
+const formatValue = ([key, value], depth) => {
+  const indent = createIndent(depth);
+  if (!_.isPlainObject(value)) {
+    return key === null
+      ? value
+      : [indent, `${key}: ${value}`].join(separator);
+  }
+  const ending = createEnding(depth);
+  const [entry] = Object.entries(value);
+  return [
+    openSymbol,
+    formatValue(entry, depth + offset),
+    ending,
+  ].join(lineBreakSymbol); // здесь без join() никак, потому что как я потом
+  // в самом конце залезу внутрь значений, чтобы их склеить, да и нужно ли это?
+};
+
+export default (data) => {
+  const iter = (tree, depth) => tree.flatMap((node) => {
+    const {
+      name,
+      type,
+      value,
+      value1,
+      value2,
+      children,
+    } = node;
+    const ending = createEnding(depth);
+    switch (type) {
+      case 'added':
+      case 'removed':
+      case 'same': {
+        return `${createPrefix(name, type, depth)}: ${formatValue([null, value], depth)}`;
+      }
+      case 'differs': {
+        const firstEntry = `${createPrefix(name, 'removed', depth)}: ${formatValue([null, value1], depth)}`;
+        const secondEntry = `${createPrefix(name, 'added', depth)}: ${formatValue([null, value2], depth)}`;
+        return [firstEntry, secondEntry];
+      }
+      case 'parent':
+        return [
+          `${createPrefix(name, type, depth)}: ${openSymbol}`,
+          ...iter(children, depth + offset),
+          ending,
+        ];
+      default:
+        throw new Error(`Unexpected node type: ${type}`);
+    }
+  });
+  return [openSymbol, iter(data, 1).join(lineBreakSymbol), closingSymbol].join(lineBreakSymbol);
+};
